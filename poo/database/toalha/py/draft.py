f089bdc125b4373e79001a36c34a7f1285d91fae
vitoriavweb <124357303+vitoriavweb@users.noreply.github.com>
2025-09-17 14:56:15 +0000

    summit

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,15 @@
+class Towel: 
+    def __init__(self):
+        self.color: str = " "
+        self.size: str = " "
+        self.wetness: int= 0 
+
+#referencias
+minha = Towel() #obj
+minha.color = "blue"
+minha.color = "P"
+
+esposa = Towel()
+esposa.color = "red"
+esposa.size = "G"
+
